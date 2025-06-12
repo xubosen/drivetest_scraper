d6c95cd9db34396d0c97bb1c0ec7a6b69f784fe0
Simon <bosen.xu@mail.utoronto.ca>
2025-06-12 18:28:17 +0800

    Edited question_scraper_test.py to group tests into a class

--- test/scraper_test/question_scraper_test.py
+++ test/scraper_test/question_scraper_test.py
@@ -10,21 +10,25 @@ QUESTION_IDS = ["c6219", "d150f", "ad9e0", "b5211", "cd86b"]
 IMG_PATH = "db_test/img"
 CONFIG_PATH = "scraper_test/site_info.json"
 
-def test_format_url():
+class TestQuestionScraper:
     """
-    Test the URL formatting function.
+    Test class for the QuestionScraper.
     """
-    scraper = QuestionScraper(IMG_PATH, CONFIG_PATH)
-    q_id = "12345"
-    expected_url = "https://tiba.jsyks.com/Post/12345.htm"
-    assert scraper._format_url(q_id) == expected_url, "URL formatting failed."
+    def test_format_url(self):
+        """
+        Test the URL formatting function.
+        """
+        scraper = QuestionScraper(IMG_PATH, CONFIG_PATH)
+        q_id = "12345"
+        expected_url = "https://tiba.jsyks.com/Post/12345.htm"
+        assert scraper._format_url(q_id) == expected_url, "URL formatting failed."
 
-def test_get_webpage():
-    """
-    Test the webpage fetching function.
-    """
-    scraper = QuestionScraper(IMG_PATH, CONFIG_PATH)
-    url = scraper._format_url("c6219")
-    soup = scraper._get_webpage(url)
-    # Print the soup for debugging purposes
-    print(soup.prettify())
+    def test_get_webpage(self):
+        """
+        Test the webpage fetching function.
+        """
+        scraper = QuestionScraper(IMG_PATH, CONFIG_PATH)
+        url = scraper._format_url("c6219")
+        soup = scraper._get_webpage(url)
+        # Print the soup for debugging purposes
+        print(soup.prettify())
