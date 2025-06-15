2aa8dfbb952c962d838e0cd3e8f4694663a11ef2
Simon <bosen.xu@mail.utoronto.ca>
2025-06-16 00:04:29 +0800

    Changed the scraper interface to use QuestionBank and logger

---
+++ src/scraper/jsyks_scraper/jsyks_scraper.py
@@ -0,0 +1,31 @@
+# Basic implementation of the scraper interface by first retrieving the
+# question ids and then scraping the questions using those ids.
+from logging import Logger
+
+# Local Imports
+from scraper.scraper_interface import Scraper
+from scraper.jsyks_scraper._qid_scraper import QidScraper
+from scraper.jsyks_scraper._question_scraper import QuestionScraper
+from questions.question_bank import QuestionBank
+
+class JSYKSScraper(Scraper):
+    """
+    Scraper that scrapes questions from the JSYKS website.
+
+    It first retrieves question IDs and then scrapes the questions using those
+    IDs.
+    """
+
+    def __init__(self, qb: QuestionBank, logger: Logger, config_path: str):
+        """
+        Initializes the JSYKSScraper with a QuestionBank and a Logger.
+
+        Args:
+            qb (QuestionBank): The question bank to store scraped questions.
+            logger (Logger): The logger to log messages.
+        """
+        self.qid_scraper = QidScraper(logger, config_path)
+        self.question_scraper = QuestionScraper(qb, logger, config_path)
+
+    def fill_question_bank(self) -> None:
+        pass
--- src/scraper/scraper_interface.py
+++ src/scraper/scraper_interface.py
@@ -1,1 +1,27 @@
 # Public interface for the scrapers
+from abc import ABC, abstractmethod
+from logging import Logger
+
+from questions.question_bank import QuestionBank
+
+class Scraper(ABC):
+    """
+    Abstract base class for scrapers.
+    """
+    @abstractmethod
+    def __init__(self, qb: QuestionBank, logger: Logger):
+        """
+        Initializes the scraper with a question bank and a logger.
+
+        Args:
+            qb (QuestionBank): The question bank to use.
+            logger (Logger): The logger to use for logging.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def fill_question_bank(self) -> None:
+        """
+        Abstract method to fill the question bank.
+        """
+        raise NotImplementedError
