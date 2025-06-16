a598f86ed907e0e2bd8df0fdb0b2f4ba23946873
Simon <bosen.xu@mail.utoronto.ca>
2025-06-16 23:13:53 +0800

    Added console_printer.py as a basic way of viewing the question bank.

--- src/data_storage/database/local_json_db.py
+++ src/data_storage/database/local_json_db.py
@@ -1,2 +1,2 @@
 # Database that stores text data in a local JSON file and image data in a local
-# directory with addresses stored in the same JSON file
+# directory with addresses stored in the aforementioned JSON file
