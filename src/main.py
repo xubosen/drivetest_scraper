64e37cd5cb7d1564f2fadd68ea95faa99abb77e1
Simon <bosen.xu@mail.utoronto.ca>
2025-06-20 08:04:12 +0800

    Added flow to view questions from database.

--- src/main.py
+++ src/main.py
@@ -4,8 +4,12 @@ from logging import Logger
 import os
 from datetime import datetime
 
+from data_storage.database.database_interface import Database
 from data_storage.database.local_json_db import LocalJsonDB
 from data_storage.in_memory.question_bank import QuestionBank
+from ui.question_displayer import QuestionDisplayer
+from ui.console_qdis import ConsoleQuesShower
+from scraper.scraper_interface import Scraper
 from scraper.jsyks_scraper.jsyks_scraper import JSYKSScraper
 
 LOG_PATH = "logs"
@@ -41,19 +45,63 @@ def make_logger() -> Logger:
 
     return logger
 
-def main():
-    logger = make_logger()
-    logger.info("Setting up the database...")
-    db = LocalJsonDB(DB_PATH, DB_IMG_DIR)
+def scrape(db: Database, logger: Logger):
     logger.info("Creating empty question bank...")
     qb = QuestionBank(IN_MEM_IMG_DIR)
     logger.info("Initializing the scraper...")
-    scraper = JSYKSScraper(qb, logger)
+    scraper: Scraper = JSYKSScraper(qb, logger)
     logger.info("Starting the scraping process...")
     scraper.fill_question_bank()
     logger.info("Scraping completed. Saving to the database...")
     db.save(qb)
-    logger.info("Scraping completed.")
+    logger.info("Questions successfully downloaded to database.")
+
+def main():
+    logger = make_logger()
+    choice = ""
+    while choice != "e":
+        choice = input("This is a simple scraper for scraping chinese drive "
+                       "test questions. \n"
+                       "Type (s) to scrape. \n"
+                       "Type (v) to view existing question bank.\n"
+                       "Type (e) to exit. \n"
+                       "Input: ").strip().lower()
+
+        logger.info("Setting up the database...")
+        db: Database = LocalJsonDB(DB_PATH, DB_IMG_DIR)
+
+        if choice == "s":
+            scrape(db, logger)
+            print("Scraping complete.")
+        elif choice == "v":
+            qb = db.load()
+            q_presenter: QuestionDisplayer = ConsoleQuesShower()
+            print("Which Chapter would you like to view? \n")
+            for chapter_num in qb.get_all_chapter_num():
+                print(f"Chapter {chapter_num}: "
+                      f"{qb.describe_chapter(chapter_num)}")
+            chapter = input("Type the chapter number here: ")
+            if chapter.isdigit() and int(chapter) in qb.get_all_chapter_num():
+                question_ids = qb.get_qids_by_chapter(int(chapter))
+                if not question_ids:
+                    print("No questions found for this chapter.")
+                else:
+                    for qid in question_ids:
+                        print(f"Displaying question with ID: {qid}")
+                        question = qb.get_question(qid)
+                        q_presenter.display_question(question)
+                        view_ans = input("Type (a) to view the answer. \n"
+                                         "Type any other key to skip. \n"
+                                         "Input: ").strip().lower()
+                        if view_ans == "a":
+                            q_presenter.display_answer(question)
+                            _ = input("Type any key to view next question.\n"
+                                      "Input: ")
+        elif choice == "e":
+            print("Closing application.")
+        else:
+            print("Unrecognized input. Please try again.")
+
 
 if __name__ == "__main__":
     main()
